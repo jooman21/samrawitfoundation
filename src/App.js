f4910f3c141132405023fc65a8b139ac752df96c
jooman21 <jooman2121@gamil.com>
2023-12-25 10:33:59 +0300

    sam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import "./App.css";
 import Gallaries from './pages/Gallaries'
 import Samrawit from './pages/Samrawit'
 import TeamMembers from './pages/TeamMembers'
 import Footer from "./components/footer/Footer";
 import Scrollup from "./components/scrollup/Scrollup";
-
+import Foundation from "./components/Foundations/Foundation";
+import About from "./components/About/About";
+import Contact from "./components/contact/Contact";
+import Teams from "./components/teams/Teams";
 function App() {
   return (
   <>
-     <HashRouter>
+     {/* <HashRouter> */}
       <Routes>
         <Route  path="/" exact  element={<Samrawit />}></Route>
         <Route  path="/galleries" element={<Gallaries />}></Route>
         <Route  path="/teamembers" element={<TeamMembers />}></Route>
+        <Route  path="/foundation" element={<Foundation />}></Route>
+        <Route  path="/about" element={<About />}></Route>
+        <Route  path="/contact" element={<Contact />}></Route>
+        <Route  path="/teams" element={<Teams />}></Route>
+
       </Routes>
 
       <Scrollup />
       <footer className="appjsfooter"><Footer /></footer>
-      </HashRouter>
+      {/* </HashRouter> */}
       
     </>
   );
