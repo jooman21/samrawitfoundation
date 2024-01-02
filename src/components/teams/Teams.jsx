71d5a87173a01004fe1009a9b3812429c9df6a61
jooman21 <jooman2121@gamil.com>
2024-01-02 11:44:40 +0300

    on-the-final

--- src/components/teams/Teams.jsx
+++ src/components/teams/Teams.jsx
@@ -21,7 +21,7 @@ function Teams() {
           
           <article className="team__member">
             <div className="team__member-img">
-              <img className = "pics" src={process.env.PUBLIC_URL +'/assets/Teams/team-22.jpg'} alt='home-img' />
+              <img className = "pics" src={process.env.PUBLIC_URL +'/assets/Teams/team-22.JPG'} alt='home-img' />
             </div>
 
             <div className="team__member-info">
